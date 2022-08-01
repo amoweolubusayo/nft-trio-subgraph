853eaa15b1df72496b4d76933830b5a2a0908f4d
Olubusayo Amowe <amoweolubusayo@gmail.com>
2022-08-01 14:28:58 +0100

    handle transfer single

--- src/proof-of-beauty-util.ts
+++ src/proof-of-beauty-util.ts
@@ -23,49 +23,13 @@ export function getOrCreateCollection(address: Address): Collection {
     collection.collectionAddress = address;
 
     let contract = ProofOfBeauty.bind(address);
-    let nameResult = contract.try_baseMetadataURI();
-    if (!nameResult.reverted) {
-      collection.collectionName = nameResult.value;
-    }
-
-    let symbolResult = contract.try_baseMetadataURI();
-    if (!symbolResult.reverted) {
-      collection.collectionSymbol = symbolResult.value;
-    }
-
-    let balanceResult = contract.try_balanceOf(address, BigInt.fromString("1"));
-    if (!balanceResult.reverted) {
-      collection.collectionBalance = balanceResult.value;
-    }
+    let name = "ProofOfBeauty";
+    collection.collectionName = name;
+    let symbol = "POB";
+    collection.collectionSymbol = symbol;
+    collection.collectionAddress = address;
 
     collection.save();
   }
   return collection;
-}
-
-export function getOrCreateCollectible(
-  collectionAddress: Bytes,
-  collectionId: string,
-  tokenId: BigInt,
-  creatorId: string,
-  createdTimestamp: BigInt
-): Collectible {
-  let collectibleId = collectionAddress
-    .toHexString()
-    .concat("-")
-    .concat(tokenId.toHexString());
-  let collectible = Collectible.load(collectibleId);
-  if (!collectible) {
-    collectible = new Collectible(collectibleId);
-    collectible.tokenId = tokenId;
-    collectible.collection = collectionId;
-    collectible.creator = creatorId;
-    collectible.owner = creatorId;
-    collectible.created = createdTimestamp;
-    collectible.descriptorUri = ProofOfBeauty.bind(
-      Address.fromBytes(collectionAddress)
-    ).contractURI();
-    collectible.save();
-  }
-  return collectible;
 }
--- src/proof-of-beauty.ts
+++ src/proof-of-beauty.ts
@@ -1,47 +1,68 @@
 import { Address } from "@graphprotocol/graph-ts";
-import { TransferSingle } from "../generated/ProofOfBeauty/ProofOfBeauty";
-import { Collectible } from "../generated/schema";
+import {
+  TransferSingle,
+  ProofOfBeauty,
+} from "../generated/ProofOfBeauty/ProofOfBeauty";
+import { Account, Collectible, Collection } from "../generated/schema";
 
 import {
   getOrCreateAccount,
-  getOrCreateCollectible,
   getOrCreateCollection,
 } from "./proof-of-beauty-util";
 
+// export function handleTransferSingle(event: TransferSingle): void {
+//   let collection = getOrCreateCollection(event.address);
+//   let receiver = getOrCreateAccount(event.params._to);
+//   if (
+//     event.params._from ==
+//     Address.fromString("0x0000000000000000000000000000000000000000")
+//   ) {
+//     // THIS IS A MINT
+//     getOrCreateCollectible(
+//       collection.collectionAddress,
+//       collection.id,
+//       event.params._id,
+//       receiver.id,
+//       event.block.timestamp
+//     );
+//   } else {
+//     let collectibleId = collection.collectionAddress
+//       .toHexString()
+//       .concat("-")
+//       .concat(event.params._id.toHexString());
+//     let collectible = Collectible.load(collectibleId);
+//     if (collectible) {
+//       if (
+//         event.params._to ==
+//         Address.fromString("0x0000000000000000000000000000000000000000")
+//       ) {
+//         // THIS IS A BURN
+//         collectible.removed = event.block.timestamp;
+//       } else {
+//         let sender = getOrCreateAccount(event.params._from);
+//         collectible.owner = sender.id;
+//         collectible.modified = event.block.timestamp;
+//       }
+//       collectible.save();
+//     }
+//   }
+// }
+
 export function handleTransferSingle(event: TransferSingle): void {
-  let collection = getOrCreateCollection(event.address);
-  let receiver = getOrCreateAccount(event.params._to);
-  if (
-    event.params._from ==
-    Address.fromString("0x0000000000000000000000000000000000000000")
-  ) {
-    // THIS IS A MINT
-    getOrCreateCollectible(
-      collection.collectionAddress,
-      collection.id,
-      event.params._id,
-      receiver.id,
-      event.block.timestamp
-    );
-  } else {
-    let collectibleId = collection.collectionAddress
-      .toHexString()
-      .concat("-")
-      .concat(event.params._id.toHexString());
-    let collectible = Collectible.load(collectibleId);
-    if (collectible) {
-      if (
-        event.params._to ==
-        Address.fromString("0x0000000000000000000000000000000000000000")
-      ) {
-        // THIS IS A BURN
-        collectible.removed = event.block.timestamp;
-      } else {
-        let sender = getOrCreateAccount(event.params._from);
-        collectible.owner = sender.id;
-        collectible.modified = event.block.timestamp;
-      }
-      collectible.save();
-    }
+  let tokenId = event.params._id;
+  let id = event.address.toHex() + "_" + tokenId.toString();
+  let contractId = event.address.toHex();
+
+  let contract = ProofOfBeauty.bind(event.address);
+  let tokenContract = Collection.load(contractId);
+  if (tokenContract == null) {
+    tokenContract = new Collection(contractId);
+
+    let name = "ProofOfBeauty";
+    tokenContract.collectionName = name;
+    let symbol = "POB";
+    tokenContract.collectionSymbol = symbol;
+    tokenContract.collectionAddress = contract._address;
+    tokenContract.save();
   }
 }
